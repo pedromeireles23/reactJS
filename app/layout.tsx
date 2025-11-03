5a841508bd1670bf05d9191735a65ee7a2221868
pedromeireles23 <pemeirels@gmail.com>
2025-11-03 16:59:19 -0300

    react useActionState com formAction para criação do formulário

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import "easymde/dist/easymde.min.css";
+import { Toaster } from "sonner";
 
 const workSans = localFont({
   src: [
